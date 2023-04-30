ff5c4d9f0295bd2261ecb144493f41345c0857a6
fvzevrvze cezceacez <vicous@free.fr>
2023-04-30 21:14:53 +0200

    first commit

---
+++ models.js
@@ -0,0 +1,10 @@
+class Board {
+    constructor() {
+      this.row1 = ["R","C","B"];
+      this.row2 = ["P","P","P","P","P","P","P","P"];
+      this.row2 = ["X","X","X","X","X","X","X","X"];
+      
+    }
+}
+
+export default Board
---
+++ script.js
@@ -0,0 +1,11 @@
+// R = Rook
+// B = Bishop
+// H = Horse
+// Q = Queen
+// K = King
+// P = Pawn
+
+import { Board } from "./models"
+
+let truc = new Board()
+console.log(truc)
